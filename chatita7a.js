33959190c000b544b10890e0654f2db879689e60
https://ita7a.com <80185284+mu744mu@users.noreply.github.com>
2022-05-26 03:34:26 +0300

    Create chatita7a.js

---
+++ chatita7a.js
@@ -0,0 +1,109 @@
+$('.loginstat').removeClass('fl').addClass('fr').css({"margin-right":"2px","margin-left":"-12px"}).appendTo('a.label.label-primary.fl')
+
+$(`	<link href="\u0068\u0074\u0074\u0070\u0073\u003A\u002F\u002F\u0063\u0064\u006E\u002E\u0073\u0074\u0061\u0074\u0069\u0063\u0061\u006C\u006C\u0079\u002E\u0069\u006F\u002F\u0067\u0068\u002F\u006D\u0075\u0037\u0034\u0034\u006D\u0075\u002F\u0043\u0068\u0061\u0074\u0032\u0074\u002F\u006D\u0061\u0069\u006E\u002F\u006E\u0065\u0077\u002E\u0063\u0073\u0073" rel="stylesheet">
+<style>
+.lonline>.hand.nosel.fl.uzr.border>div>div:first-child {
+    max-height: 28px;
+}
+.uzr.fl.corner.borderg.mm {
+    background-color: #ffffff3b!important;
+}
+.lonline>.hand.nosel.fl.uzr.border>div {
+    width: 82%!important;
+}
+.lonline>.hand.nosel.fl.uzr.border>div>.fl.mini.u-msg {
+    color: #454545!important;
+    text-align: center;
+    background-color: #EBEBEB;
+}
+.lonline>.hand.nosel.fl.uzr.border {
+    margin: 0px 1px 1px 2px!important;
+    padding: 1px;
+    border: none;
+    border-left: 1px solid #262161;
+    border-right: 1px solid #262161;
+    border-radius: 3px!important;
+}
+			ul.nav.nav-tabs li a {
+    width: 100%;
+    text-align: center;
+}
+			ul.nav.nav-tabs li {
+    width: 33%;
+}
+div#tlogins>a {
+    font-family: 'MuHanad',FontAwesome;
+}
+div#tlogins .fr.borderg{
+display: none;
+}
+div#tlogins .ssss,div#tlogins .fa.fa-refresh {
+    border-radius: 0;
+}
+div#tlogins a.label.label-primary.fl img {
+    border-radius: 12%;
+    border: 1px solid #fff;
+margin-top: -2px;
+}
+input#u3,input#u2 {
+    margin-left: 2px;
+}
+div#tlogins div#l1 button ,div#tlogins div#l2 button,div#tlogins div#l3 button {
+    width: 50px;
+}
+		</style>
+<div class="rainbow"><img alt="شات التحليه" src="\x68\x74\x74\x70\x73\x3A\x2F\x2F\x69\x74\x61\x37\x61\x2E\x63\x6F\x6D\x2F\x73\x69\x63\x6F\x2F\x31\x36\x35\x33\x30\x38\x32\x30\x37\x34\x35\x35\x38\x2E\x67\x69\x66" class="fl" style="width:100%;margin-bottom: 2px;background-color: white;"></div><a href="#" target="_blank" style="background: white;padding: 2px 0;width: 24%;font-size: 14px!important;border-radius: 50px  0;margin-right: 2px;border: none;border-top: 2px solid;border-bottom: 2px solid;font-family: 'jazeera-light',FontAwesome;" type="a" class="fr primaryborder btn btn-default">
+        اتصل بنا
+<i style="margin-left: 2px;" class="fa fa-star-o"></i>
+</a>
+<a href="https://ita7a.com/" target="_blank" type="a" style="background: white;padding: 2px 0;width: 24%;font-size: 14px!important;border: none;border-top: 2px solid;border-bottom: 2px solid;border-radius: 0 50px ;margin-left: 5px;font-family: 'jazeera-light',FontAwesome;" class="fr primaryborder btn btn-default">
+    زخارف
+<i style="margin-left: 2px;" class="fa fa-heart-o"></i>
+</a>
+<a href="https://ita7a.com/" target="_blank" type="a" style="background: white;padding: 2px 0;width: 24%;font-size: 14px!important;border-radius: 50px  0;border: none;border-top: 2px solid;border-bottom: 2px solid;font-family: 'jazeera-light',FontAwesome;" class="fr primaryborder btn btn-default">
+        اشتراكات
+<i style="margin-left: 2px;" class="fa fa-eye"></i>
+</a>
+<a href="https://ita7a.com/" target="_blank" style="background: white;padding: 2px 0;width: 24%;font-size: 14px!important;border: none;border-top: 2px solid;border-bottom: 2px solid;border-radius: 0 50px ;font-family: 'jazeera-light',FontAwesome;" type="a" class="fr primaryborder btn btn-default">
+        القوانين
+<i style="margin-left: 2px;" class="fa fa-comments-o"></i>
+    </a>
+
+`).insertBefore('.nav-tabs');
+$('.hand.nosel.fl.uzr.border').css({'margin': '5px 5',borderRight: '8px solid #FFFFFF',borderLeft: '7px solid #FFFFFF',borderTop: '5px solid #FFFFFF',borderBottom: '2px solid #E9EEF7',paddingTop: '5px',borderRadius: '7px'});
+('div#tlogins div#l1 input,div#tlogins div#l2 input,div#tlogins div#l3 input').addClass('form-control primaryborder').css({'text-align':'center','max-width':'40%','float': 'right'})
+$('div#l1 br,div#l2 br,div#l3 br').remove()
+$('input#stealth').css('max-width','15px').removeClass('form-control')
+$('#l2 .checkbox label').html($('#l2 .checkbox label input')[0].outerHTML)
+$('#l2 .checkbox').css({"margin":"7px -5px 0 0","float":"right"})
+$('ul.nav.nav-tabs,.nav-tabs>li>a').addClass('label-primary').css({"color":"white","padding":"4px 0px"})
+$('ul.nav.nav-tabs').css({"padding":"0px"})
+$('input#u1').css({"max-width":"80%"})
+$('.lonline.light.break').addClass('primaryborder').css({"border-top":"2px solid","padding-top":"5px"})
+
+$(`<div style=" padding: 3px;margin: 2px; border: 1px solid #D6E3E3;height: 29px;width: 106px;" class="btn " type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">الادارة</div>
+<div class="collapse" id="collapseExample"><div style="text-align: center;font-family: 'jazeera';color: #C91212;" class="well">
+<p class="bg-primary">｢ MuHanad ｣｢<p class="bg-primary">｢للشكاوي والاقتراحات ｣｢</p><p class="bg-primary">｢الماس الفيصل ｣｢</p>｢ الادارة ｣｢</div> 
+</div>`).insertBefore('a.fl.fa.fa-comment.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.upm.borderg');
+
+
+$("div#tlogins button.btn.btn-primary").click(function(){
+var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){
+   $(`
+    <div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
+      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url();" class="fl fitimg hand u-pic    ">
+      <div class="uzr fl" style="padding:0px;width:80%">
+        <div style="padding:0px;width:100%;" class="fl">
+          <img class="fl u-ico" alt="" src="">
+          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: E00000(199, 103, 48);" class="corner nosel u-topic dots fl hand"> ｢ اداره الموقــع ｣｢</span>
+        </div>
+        <br>
+        <div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg   break  fl">
+        <div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg   break  fl"> مرحباً بك 👋👋👋👋 <h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1> نقدّر تواجدك معنا في شات التحليه ونتمنى لك يوماً سعيداً إن شاء الله </div>
+      </div>
+    </div></div>
+`).appendTo('div#d2')
+clearInterval(myVar);
+
+}else{console.log(usmsgw)} }, 2000);
+})
